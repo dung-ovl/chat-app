bd5664807665a2eee238b78e0be6340e9b37e853
UIT-20521205 <dinhiendung1026@gmail.com>
2024-04-30 18:46:59 +0700

    nav bar

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,12 +1,22 @@
+import { Stack } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import useResponsive from "../../hooks/useResponsive";
+import SideNav from "./SideNav";
 
 const DashboardLayout = () => {
+  const isDesktop = useResponsive("up", "md");
 
   return (
     <>
-      Dashboard Layout
-      <Outlet />
+      <Stack direction="row">
+        {isDesktop && (
+          // SideBar
+          <SideNav />
+        )}
+
+        <Outlet />
+      </Stack>
     </>
   );
 };
